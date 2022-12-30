d29f1d786964b9d0acd048e15075d75f722bb307
Girafa3456 <goncalo.monteiro@ua.pt>
2022-12-30 14:41:43 +0000

    ...

---
+++ Interface/competitionDetails.js
@@ -0,0 +1,105 @@
+// ViewModel KnockOut
+var vm = function () {
+    console.log('ViewModel initiated...');
+    //---Variáveis locais
+    var self = this;
+    self.baseUri = ko.observable('http://192.168.160.58/Olympics/api/Competitions/');
+    self.displayName = 'Olympic Competitions edition Details';
+    self.error = ko.observable('');
+    self.passingMessage = ko.observable('');
+    //--- Data Record
+    self.Id = ko.observable('');
+    self.Name = ko.observable('');    
+    self.CityName = ko.observable('');
+    self.CountryName = ko.observable('');
+    self.Lat = ko.observable('');
+    self.Lon = ko.observable('');
+    self.Logo = ko.observable('');
+    self.Photo = ko.observable('');
+    self.Season = ko.observable('');
+
+    self.Url = ko.observable('');
+
+    //--- Page Events
+    self.activate = function (id) {
+        console.log('CALL: getGame...');
+        var composedUri = self.baseUri() + id;
+        ajaxHelper(composedUri, 'GET').done(function (data) {
+            console.log(data);
+            hideLoading();
+            self.Id(data.Id);
+            self.Name(data.Name);    
+            self.CityName(data.CityName);        
+            self.CountryName(data.CountryName);
+            self.Lat(data.Lat);
+            self.Lon(data.Lon);
+            self.Year(data.Year);
+            self.Logo(data.Logo);            
+            self.Photo(data.Photo);            
+        });
+    };
+
+    //--- Internal functions
+    function ajaxHelper(uri, method, data) {
+        self.error(''); // Clear error message
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null,
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("AJAX Call[" + uri + "] Fail...");
+                hideLoading();
+                self.error(errorThrown);
+            }
+        });
+    }
+
+    function showLoading() {
+        $('#myModal').modal('show', {
+            backdrop: 'static',
+            keyboard: false
+        });
+    }
+    function hideLoading() {
+        $('#myModal').on('shown.bs.modal', function (e) {
+            $("#myModal").modal('hide');
+        })
+    }
+
+    function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    };
+
+    //--- start ....
+    showLoading();
+    var pg = getUrlParameter('id');
+    console.log(pg);
+    if (pg == undefined)
+        self.activate(1);
+    else {
+        self.activate(pg);
+    }
+    console.log("VM initialized!");
+};
+
+$(document).ready(function () {
+    console.log("document.ready!");
+    ko.applyBindings(new vm());
+});
+
+$(document).ajaxComplete(function (event, xhr, options) {
+    $("#myModal").modal('hide');
+})
