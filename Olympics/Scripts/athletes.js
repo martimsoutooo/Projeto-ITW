a9bc5ac2f8402a9d44de9665c7c78d23dc9c15d3
martimsoutooo <martim15souto@icloud.com>
2023-01-04 00:05:23 +0000

    something

--- Olympics/Scripts/athletes.js
+++ Olympics/Scripts/athletes.js
@@ -1,40 +1,3 @@
-/*
-// ViewModel KnockOut
-var vm = function () {
-    console.log('ViewModel initiated...');
-    //---Variáveis locais
-    var self = this;
-    self.baseUri = ko.observable('http://192.168.160.58/Olympics/api/Athletes/');
-    self.records = ko.observableArray([]);
-    self.athletes = function(){
-        if (self.records().length < 48){
-            let sebastiao = Math.floor(Math.random() * 135000) + 1;
-            $.ajax({
-                url:self.baseUri() + sebastiao,
-                type: 'GET',
-                timeout: 15000, 
-                success:function(data){
-                if (data["Photo"] != null){
-                    self.records.push(data)
-                    self.athletes();
-                }}
-            
-            })}
-    }
-    self.getrecords = function(){
-        return self.records()
-    }
-}
-
-var jsp = new vm()
-$().ready(function(){
-    jsp.athletes()
-    ko.applyBindings(jsp)
-})
-function martim(){
-    return jsp.getrecords()
-}
-*/
 // ViewModel KnockOut
 var vm = function () {
     console.log('ViewModel initiated...');
@@ -51,7 +14,6 @@ var vm = function () {
     self.totalRecords = ko.observable(135571);
     self.hasPrevious = ko.observable(false);
     self.hasNext = ko.observable(true);
-    
     self.Name = ko.observable('');
     self.Photo = ko.observable('')
     self.previousPage = ko.computed(function () {
@@ -97,12 +59,7 @@ var vm = function () {
             self.pagesize(data.PageSize)
             self.totalPages(data.TotalPages);
             self.totalRecords(data.TotalRecords);
-            if (data.Photo == null) {
-                self.Photo('./Images/anyone.jpg')
-            } else {
-                self.Photo(data.Photo);
-            }
-            //self.SetFavourites();
+            self.Photo(data.Photo);
         });
     };
 
@@ -174,4 +131,4 @@ $(document).ready(function () {
 
 $(document).ajaxComplete(function (event, xhr, options) {
     $("#myModal").modal('hide');
-})
+})};
