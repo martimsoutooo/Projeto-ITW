ee815d23c6cb171363de8620f163b340a345fb86
martimsoutooo <martim15souto@icloud.com>
2023-01-05 14:54:23 +0000

    change marker

--- Olympics/Scripts/index.js
+++ Olympics/Scripts/index.js
@@ -30,7 +30,7 @@ function activate() {
             var Lat=parseFloat(data[id].Lat);
             var Lon=parseFloat(data[id].Lon);
             var myIcon = new L.Icon({
-                iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+                iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
                 shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
                 iconSize: [25, 41],
                 iconAnchor: [12, 41],
